771a32bf1611ed86afbb49a9f6c76395d7649f20
Kirill Brodt <cyrill.brodt@gmail.com>
2021-07-20 00:32:47 +0700

    Add scripts to preprocess data, train the model and infer

---
+++ dist_train.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+GPU=${GPU:-0,1,2,3}
+PORT=${PORT:-29500}
+N_GPUS=4
+BACKBONE=efficientnet-b5
+ENCODER_WEIGHTS=imagenet
+BATCH_SIZE=8
+NUM_EPOCHS=1025
+DATASET_DIR=./data/train_tif
+TRAIN_DF=./data/geopose_train.csv
+RGB_SUFFIX=tif
+UNIT=m
+N_FOLDS=40
+FOLD=2
+OPTIM=fusedadam
+LR=0.0001
+SCHEDULER=cosa
+MODEL_TYPE=unet
+CHECKPOINT=./chkps_dist/${BACKBONE}/${FOLD}_aug_ft_${MODEL_TYPE}  # /${CITY}
+# LOAD=./chkps_dist/${BACKBONE}/${FOLD}_aug_${MODEL_TYPE}/model_last.pth  # ${CITY}
+# LOAD=${CHECKPOINT}/model_last.pth
+LMDB=./data/train.lmdb
+
+
+CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --nproc_per_node=${N_GPUS} --master_port=${PORT} \
+    ./overhead-geopose-challenge/main.py \
+    --train \
+    --use-city \
+    --fp16 \
+    --agl-weight=2 \
+    --angle-weight=50  \
+    --scale-weight=50  \
+    --lmdb=${LMDB} \
+    --model-type ${MODEL_TYPE} \
+    --backbone ${BACKBONE} \
+    --encoder-weights ${ENCODER_WEIGHTS} \
+    --optim ${OPTIM} \
+    --learning-rate ${LR} \
+    --scheduler ${SCHEDULER} \
+    --T-max 25 \
+    --num-epochs=${NUM_EPOCHS} \
+    --checkpoint-dir=${CHECKPOINT} \
+    --dataset-dir=${DATASET_DIR} \
+    --train-path-df=${TRAIN_DF} \
+    --batch-size=${BATCH_SIZE} \
+    --num-workers=${BATCH_SIZE} \
+    --rgb-suffix=${RGB_SUFFIX} \
+    --unit=${UNIT} \
+    --n-folds=${N_FOLDS} \
+    --fold=${FOLD} \
+    --lmdb=${LMDB} \
+    --distributed
+
+    # --load=${LOAD}
+    # --augmentation \
+    # --albu
+    # --resume
